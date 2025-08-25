4505c927fd7097a88ab02bd98980991ee77bb1f6
Kulathunga D.M.D.T. <kulathungadmdt.21@uom.lk>
2025-08-25 17:39:49 +0530

    Signup page is created

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 import Navbar from "./assets/components/Navbar";
 import Footer from "./assets/components/Footer";
 import HomePage from "./assets/pages/HomePage";
-//import SignupPage from "./assets/pages/SignupPage";
+import SignupPage from "./assets/pages/SignupPage";
 //import LoginPage from "./assets/pages/LoginPage";
 //import Statistics from "./assets/pages/Statistics";
 
@@ -15,8 +15,8 @@ function App() {
         <Navbar />
         <main className="main-content">
           <Routes>
-            {/*<Route path='/signup' element={<SignupPage/>}/>
-            <Route path="/login" element={<LoginPage />} /> 
+            <Route path='/signup' element={<SignupPage/>}/>
+           {/* <Route path="/login" element={<LoginPage />} /> 
             <Route path='/statistics' element={<Statistics/>}/> */}
             <Route path="/" element={<HomePage />} />
           </Routes>
